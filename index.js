9d817f01d4e05ea2adf8fa6d2927f742657eb726
Agustin Sanchez <agus.sanchez.240@gmail.com>
2022-12-27 02:41:44 -0300

    Add DOM events and refactor

--- index.js
+++ index.js
@@ -1,50 +1,135 @@
 function getComputerChoice() {
   let items = ["rock", "paper", "scissors"];
-  let computerSelection = items[Math.floor(Math.random() * items.length)];
+  let computerSelectionItem = items[Math.floor(Math.random() * items.length)];
 
-  return computerSelection;
+  return computerSelectionItem;
 }
 
-function getPlayerChoice() {
-  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+let playerSelection;
+let roundResult = "";
+let playerScore = 0;
+let computerScore = 0;
 
-  return playerSelection;
-}
-
-function playRound() {
-  computerSelection = getComputerChoice();
-  playerSelection = getPlayerChoice();
-  let roundItem = "";
-
-  if (playerSelection === computerSelection) {
-    roundItem = "It's a Draw";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    roundItem = "You win! Rock beats Scissors";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    roundItem = "You lose! Rock beats Scissors";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    roundItem = "You win! Paper beats Rock";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    roundItem = "You lose! Paper beats Rock";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    roundItem = "You win! Scissors beats Paper";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    roundItem = "You lose! Scissors beats Paper";
-  } else {
-    roundItem = "Invalid input";
+function score() {
+  switch (roundResult) {
+    case "You win! Rock beats Scissors":
+    case "You win! Paper beats Rock":
+    case "You win! Scissors beats Paper":
+      playerScore++;
+      break;
+    case "You lose! Rock beats Scissors":
+    case "You lose! Paper beats Rock":
+    case "You lose! Scissors beats Paper":
+      computerScore++;
+      break;
   }
 
-  console.log("You have chosen " + playerSelection);
-  console.log("The computer choice " + computerSelection);
-  console.log(roundItem);
+  return playerScore && computerScore;
 }
+tetxContainer = document.querySelector(".playround-results");
+
+function playRound() {
+  let computerSelection = getComputerChoice();
+  playerSelection;
+
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    roundResult = "You win! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    roundResult = "You lose! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    roundResult = "You win! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    roundResult = "You lose! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    roundResult = "You win! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    roundResult = "You lose! Scissors beats Paper";
+  } else {
+    roundResult = "It's a Draw";
+  }
+
+  score();
+
+  let playerSelectionText = document.createElement("p");
+  playerSelectionText.textContent = "You have chosen " + playerSelection;
+  tetxContainer.appendChild(playerSelectionText);
+
+  let computerSelectionText = document.createElement("p");
+  computerSelectionText.textContent = "The computer choice " + computerSelection;
+  tetxContainer.appendChild(computerSelectionText);
+
+  let roundResultText = document.createElement("p");
+  roundResultText.textContent = roundResult;
+  tetxContainer.appendChild(roundResultText);
+}
+
+let scoreContainer = document.querySelector(".score-results");//usar la misma logica para el output de playround
+
+let playerScoreText = document.querySelector(".player-score");
+let emptyPlayerScoreSpan = document.querySelector(".player-score-num");
+
+let computerScoreText = document.querySelector(".computer-score");
+let emptyComputerScoreSpan = document.querySelector(".computer-score-num");
+
+
+let buttonPlayerSelection = document.querySelector(".btn-rock");
+buttonPlayerSelection.addEventListener("click", () => {
+  playerSelection = "rock";
+  playRound();
+  let playerScoreNum = document.createElement("span");
+  playerScoreNum.textContent = "The player score is " + playerScore;
+  playerScoreText.replaceChildren(emptyPlayerScoreSpan, playerScoreNum);
+
+  let computerScoreNum = document.createElement("span");  
+  computerScoreNum.textContent = "The computer score is " + computerScore;
+  computerScoreText.replaceChildren(emptyComputerScoreSpan, computerScoreNum);
+
+  stopGame();
+});
+
+buttonPlayerSelection = document.querySelector(".btn-paper");
+buttonPlayerSelection.addEventListener("click", () => {
+  playerSelection = "paper";
+  playRound();
+
+  let playerScoreNum = document.createElement("span");
+  playerScoreNum.textContent = "The player score is " + playerScore;
+  playerScoreText.replaceChildren(emptyPlayerScoreSpan, playerScoreNum);
+
+  let computerScoreNum = document.createElement("span");  
+  computerScoreNum.textContent = "The computer score is " + computerScore;
+  computerScoreText.replaceChildren(emptyComputerScoreSpan, computerScoreNum);
+});
+
+buttonPlayerSelection = document.querySelector(".btn-scissors");
+buttonPlayerSelection.addEventListener("click", () => {
+  playerSelection = "scissors";
+  playRound();
+
+  let playerScoreNum = document.createElement("span");
+  playerScoreNum.textContent = "The player score is " + playerScore;
+  playerScoreText.replaceChildren(emptyPlayerScoreSpan, playerScoreNum);
+
+  let computerScoreNum = document.createElement("span");  
+  computerScoreNum.textContent = "The computer score is " + computerScore;
+  computerScoreText.replaceChildren(emptyComputerScoreSpan, computerScoreNum);
+});
 
 function game() {
   for (let i = 1; i <= 5; i++) {
     playRound();
   }
 
-  console.log("Round finished")
+  console.log("Round finished");
 }
 
-game();
+function stopGame() {
+  if(playerScore === 5 || computerScore === 5 ){
+    document.write("you win"); // esto no caralfombra
+  }
+}
+
+const resetGame = document.querySelector(".reset");
+resetGame.addEventListener("click", () => {
+  location.reload();
+});
